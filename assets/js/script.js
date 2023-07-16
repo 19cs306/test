1ae513f72dc7263f6e5e80a52da746f31f905705
19cs306 <131914189+19cs306@users.noreply.github.com>
2023-07-17 00:59:54 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
+// Create a function to handle the intersection callback
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.getAttribute("id");
+    const navLink = document.querySelector(
+      `.nav-links li a[href="#${sectionId}"]`
+    );
+    if (navLink !== null) {
+      if (entry.isIntersecting) {
+        // Add the "active" class to the corresponding nav link
+        navLink.classList.add("active");
+      } else {
+        // Remove the "active" class from the nav link
+        navLink.classList.remove("active");
+      }
+    }
+  });
+}
+
+// Create the Intersection Observer instance
+const observer = new IntersectionObserver(handleIntersection, {
+  root: null, // Use the viewport as the root element
+  rootMargin: "-50% 0px", // Trigger when the top of the section is near the top of the display
+  threshold: 0, // Trigger when the section is fully visible
+});
+
+
+// Observe each section
+document.querySelectorAll("section").forEach((section) => {
+  observer.observe(section);
+});
+
 const banner = document.querySelector(".canvas");
-const navbar = document.querySelector(".navbar ")
+const navbar = document.querySelector(".navbar ");
 window.onscroll = () => {
   this.scrollY > 10
     ? banner.classList.add("hidden") | navbar.classList.add("sticky")
@@ -19,4 +51,3 @@ function SubForm() {
     },
   });
 }
-
